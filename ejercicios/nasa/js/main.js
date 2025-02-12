c3b4f49763beac9d80711b8ecfcbfbe2f716c5b6
rodrigo clase <rodrigo123456po@gmail.com>
2025-02-12 09:49:34 +0100

    12/02/2025

---
+++ ejercicios/nasa/js/main.js
@@ -0,0 +1,23 @@
+import { obtenerImagen } from "./api.js";
+
+document.getElementById("btn-ver").addEventListener("click", async () => {
+  const fecha = document.getElementById("fecha").value;
+  const datos = await obtenerImagen(fecha);
+
+  if (datos) {
+    document.getElementById("titulo").textContent = datos.title;
+    document.getElementById(
+      "fecha-seleccionada"
+    ).textContent = `Fecha: ${datos.date}`;
+    document.getElementById("explicacion").textContent = datos.explanation;
+
+    const mediaContainer = document.getElementById("media");
+    mediaContainer.innerHTML = ""; // Limpiar contenido previo
+
+    if (datos.media_type === "image") {
+      mediaContainer.innerHTML = `<img src="${datos.url}" alt="Imagen de la NASA">`;
+    } else if (datos.media_type === "video") {
+      mediaContainer.innerHTML = `<iframe src="${datos.url}" frameborder="0" allowfullscreen></iframe>`;
+    }
+  }
+});
